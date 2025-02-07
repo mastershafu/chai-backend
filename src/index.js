764103470b1f6dc7b151d8b20ccb7526e13707eb
mastershafu <sk.shafayath@gmail.com>
2025-02-08 03:43:37 +0600

    Util Operations

--- src/index.js
+++ src/index.js
@@ -9,7 +9,18 @@ import 'dotenv/config'
 import connectDB from './db/index.js'
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port: ${process.env.PORT}`);
+    })
+    app.on("error", (error) => {
+        console.log("Can not listen to the app", error);
+        throw error
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed!!", err)
+})
 
 
 
@@ -31,7 +42,7 @@ const app = express()
 
 ( async () => {
     try{
-        await mongoose.connect('${process.env.MONGODB_URI}/${DB_NAME}') 
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
         app.on("error", (error) => {
             console.log("Can not listen to the app", error);
             throw error
